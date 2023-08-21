c24eebee8901d329dfea44c010876c54b503cc7f
Kelechiswe <kalleenz070@gmail.com>
2023-08-21 06:08:56 -0700

    kalace

---
+++ kalace.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf - entry point to print format specifiers
+ * @format: list of specifiers
+ * Return: an integer
+ */
+int _printf(const char *format, ...)
+{
+}
