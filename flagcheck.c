f80426b419ae7f97d8753c38aeae79475256a1b5
Kelechiswe <kalleenz070@gmail.com>
2023-08-22 08:45:18 -0700

    checkcommit

--- flagcheck.c
+++ flagcheck.c
@@ -1,3 +1,10 @@
 #include "main.h"
 
+/**
+ * flag_check - used to check all the flag elements in the specifiers
+ * @format: pointer to the format
+ * @i: pointer to index of format
+ * Return: an integer
+ */
+
 int flag_check(const char *format, int *i)
--- widthcheck.c
+++ widthcheck.c
@@ -1,3 +1,11 @@
 #include "main.h"
 
+/**
+ * width_check - checking the width for format string
+ * @format: pointer to format
+ * @i: pointer to index of format string
+ * @list: list of specifiers
+ * Return: an integer
+ */
+
 int width_check(const char *format, int *i, va_list list)
